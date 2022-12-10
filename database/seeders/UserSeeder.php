5c305aea46352295345b4941c60bec80cc3ad5fc
ferdyhape <ferdyhahan5@gmail.com>
2022-12-10 11:50:59 +0700

    create user seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Ferdy Hahan Pradana',
+            'email' => 'ferdyhahan5@gmail.com',
+            'level' => 'admin',
+            'password' => bcrypt('password')
+        ]);
+    }
+}
