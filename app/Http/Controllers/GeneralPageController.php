081ca0210c452b23e27b8c0c1153ab5f6bf20875
ferdyhape <ferdyhahan5@gmail.com>
2022-12-12 13:07:07 +0700

    create general page controller

--- app/Http/Controllers/GeneralPageController.php
+++ app/Http/Controllers/GeneralPageController.php
@@ -2,35 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\item;
 use Illuminate\Http\Request;
+use App\Models\item_category;
+use App\Http\Controllers\Controller;
 
 class GeneralPageController extends Controller
 {
     public function index()
     {
-        return view('index', [
-            'title' => 'Home'
+        return view('general-page.index', [
+            'title' => 'Home',
+            'item' => item::orderBy('created_at', 'desc')->get(),
         ]);
     }
 
     public function about()
     {
-        return view('about', [
-            'title' => 'about'
+        return view('general-page.about', [
+            'title' => 'About Us'
         ]);
     }
 
     public function contact()
     {
-        return view('contact', [
+        return view('general-page.contact', [
             'title' => 'Contact'
         ]);
     }
 
     public function product()
     {
-        return view('product', [
-            'title' => 'Product'
+        return view('general-page.product', [
+            'title' => 'Products',
+            'category' => item_category::all(),
+            // 'item' => item::all(),
+            'item' => item::orderBy('price', 'asc')->get(),
         ]);
     }
 }
