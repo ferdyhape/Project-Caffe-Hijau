e2c6c487a35e7344c6084931d956cad5ce625dc5
ferdyhape <ferdyhahan5@gmail.com>
2023-01-08 16:11:51 +0700

    setup head for connect local assets

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -5,24 +5,11 @@
 <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap"
     rel="stylesheet">
 
-<!-- Bootstrap core CSS -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
-    integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
-    crossorigin="anonymous" referrerpolicy="no-referrer" />
-
 <!-- Additional CSS Files -->
-<link rel="stylesheet" href="assets/css/fontawesome.css">
+<link rel="stylesheet" href="assets/fontawesome-free-6.2.1-web/css/all.min.css">
+<link rel="stylesheet" href="assets/bootstrap-5.0.2-dist/css/bootstrap.min.css">
 <link rel="stylesheet" href="assets/css/templatemo-sixteen.css">
 <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
 <link rel="stylesheet" href="assets/owl.carousel.min.css">
 <link rel="stylesheet" href="assets/owl.theme.default.min.css">
 <title>{{ $title }}</title>
-
-@if (Route::is('auth'))
-{{--
-<link rel="stylesheet" href="assets/css/StyleAuth.css"> --}}
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-@endif
