2893ec7c4a395bc0d491b723da390a70d172a75f
ferdyhape <ferdyhahan5@gmail.com>
2023-01-12 02:32:43 +0700

    beautify ui add items

--- resources/views/dashboard/item/create.blade.php
+++ resources/views/dashboard/item/create.blade.php
@@ -1,31 +1,58 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-
-<body>
-    <form action="{{ url('dashboard/item') }}" method="POST" enctype="multipart/form-data">
-        @csrf
-        <label for="name">Item Name</label>
-        <input type="text" name="name"><br>
-        <label for="price">Price</label>
-        <input type="number" name="price"><br>
-        <label for="category_id">Choose a category:</label><br>
-        <select name="category_id">
-            <option value="1">Coffe</option>
-            <option value="2">Tea</option>
-        </select><br>
-        <label for="description">Description</label>
-        <input type="text" name="description"><br>
-        <label for="picture">Picture</label>
-        <input type="file" name="picture"><br>
-        <button type="submit">Add Item</button>
-    </form>
-</body>
-
 </html>
+@extends('dashboard.layouts.main')
+@section('content')
+<!-- Begin Page Content -->
+<div class="container row justify-content-center">
+    <div class="col-lg-6 mt-2 mb-4">
+        <div class="card border-0 shadow-sm">
+            <h5 class="card-header text-center text-uppercase">Add New Item</h5>
+            <div class="card-body p-5">
+                <form action="{{ url('dashboard/item') }}" method="POST" enctype="multipart/form-data">
+                    @csrf
+                    <div class="form-group">
+                        <input type="text" class="form-control form-control-user" name="name" placeholder="Item Name"
+                            autofocus>
+                    </div>
+                    <div class="form-group">
+                        <input type="number" class="form-control form-control-user" name="price" placeholder="Price">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control form-control-user" name="description"
+                            placeholder="Description">
+                    </div>
+                    <div class="form-group">
+                        <select class="form-control" id="category_id" name="category_id">
+                            <option>Category</option>
+                            @foreach($category as $c)
+                            <option value="{{ $c->id }}">{{ $c->name }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <div class="input-group">
+                        <label class="input-group-text" for="picture">Picture</label>
+                        <input type="file" class="form-control" id="picture" name="picture" onchange="previewImage()">
+                    </div>
+                    <img class="img-preview img-fluid mt-1" id="img-preview">
+                    <button type="submit" class="btn btn-primary btn-user btn-block mt-3">Add Item</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script>
+    function previewImage() {
+        const image = document.querySelector('#picture');
+        const imgPreview = document.querySelector('.img-preview')
+
+        imgPreview.style.display = 'block';
+
+        const oFReader = new FileReader();
+        oFReader.readAsDataURL(image.files[0]);
+
+        oFReader.onload = function(oFREvent) {
+            imgPreview.src = oFREvent.target.result;
+        }
+    }
+</script>
+@endsection
