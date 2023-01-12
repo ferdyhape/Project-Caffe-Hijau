2e2dbb4863080748174c65bd7ef239aebd6dac0f
ferdyhape <ferdyhahan5@gmail.com>
2023-01-13 04:31:59 +0700

    update item_category_migrations add description

--- database/migrations/2022_12_08_142644_create_item_categories_table.php
+++ database/migrations/2022_12_08_142644_create_item_categories_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('item_categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('description', 255)->nullable();
             $table->timestamps();
         });
     }
