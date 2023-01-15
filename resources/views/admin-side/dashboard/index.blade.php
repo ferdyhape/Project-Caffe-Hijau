4480d8d5fe57225e477a35112a5de5b4bc38b258
ferdyhape <ferdyhahan5@gmail.com>
2023-01-16 00:47:02 +0700

    update dashboard add count of Banner

--- resources/views/admin-side/dashboard/index.blade.php
+++ resources/views/admin-side/dashboard/index.blade.php
@@ -14,7 +14,6 @@
     <!-- Content Row -->
     <div class="row">
 
-        <!-- Earnings (Monthly) Card Example -->
         <div class="col-xl-3 col-md-6 mb-4">
             <div class="card border-left-primary shadow h-100 py-2">
                 <div class="card-body">
@@ -32,7 +31,6 @@
             </div>
         </div>
 
-        <!-- Earnings (Monthly) Card Example -->
         <div class="col-xl-3 col-md-6 mb-4">
             <div class="card border-left-success shadow h-100 py-2">
                 <div class="card-body">
@@ -55,7 +53,7 @@
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">
                                 Sum of User</div>
                             <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $userCount }}</div>
                         </div>
@@ -67,18 +65,17 @@
             </div>
         </div>
 
-        <!-- Pending Requests Card Example -->
         <div class="col-xl-3 col-md-6 mb-4">
             <div class="card border-left-warning shadow h-100 py-2">
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
                             <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
-                                Pending Requests</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
+                                Sum Of Banner</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $bannerCount }}</div>
                         </div>
                         <div class="col-auto">
-                            <i class="fas fa-comments fa-2x text-gray-300"></i>
+                            <i class="fas fa-images fa-2x text-gray-300"></i>
                         </div>
                     </div>
                 </div>
