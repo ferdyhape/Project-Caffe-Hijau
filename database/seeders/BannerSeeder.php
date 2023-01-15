501e6a9a19981cb3409a73e33f892eee0f0ed425
ferdyhape <ferdyhahan5@gmail.com>
2023-01-16 00:34:40 +0700

    update seeder add column offer, attention and fs

--- database/seeders/BannerSeeder.php
+++ database/seeders/BannerSeeder.php
@@ -17,14 +17,26 @@ class BannerSeeder extends Seeder
     {
         Banner::create([
             'name' => 'Banner 1',
+            'attention' => 'Banner 1 Attention!',
+            'fzAttention' => '35',
+            'offer' => 'Banner 1 Offer',
+            'fzOffer' => '40',
             'picture' => 'banner-picture/Banner1.jpg'
         ]);
         Banner::create([
             'name' => 'Banner 2',
+            'attention' => 'Banner 2 Attention!',
+            'fzAttention' => '35',
+            'offer' => 'Banner 2 Offer',
+            'fzOffer' => '40',
             'picture' => 'banner-picture/Banner2.jpg'
         ]);
         Banner::create([
             'name' => 'Banner 3',
+            'attention' => 'Banner 3 Attention!',
+            'fzAttention' => '35',
+            'offer' => 'Banner 3 Offer',
+            'fzOffer' => '40',
             'picture' => 'banner-picture/Banner3.jpg'
         ]);
     }
