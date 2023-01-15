676b871781048e1c2563d5f1629faf7354c665cc
ferdyhape <ferdyhahan5@gmail.com>
2023-01-16 00:35:51 +0700

    update category request add nullable validation

--- app/Http/Requests/Updateitem_categoryRequest.php
+++ app/Http/Requests/Updateitem_categoryRequest.php
@@ -25,7 +25,7 @@ class Updateitem_categoryRequest extends FormRequest
     {
         return [
             'name' => 'required|string|max:50',
-            'description' => 'max:255',
+            'description' => 'nullable|string|max:255',
         ];
     }
     public function messages()
