70e531ef572a4a1ad4d535e6bc38f6caf721a517
Atmayanti <mayablue74@gmail.com>
2023-01-17 19:17:39 +0700

    login scenario - TC-002

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -9,17 +9,33 @@ describe('login scenario', () => {
         cy.visit('/login')
     })
 
-    //TC-001
-    it('user can access login page', () => {
-        cy.get('.h4').contains('Welcome Back!')
-        cy.get(':nth-child(2) > .form-control').should('have.attr', 'placeholder', 'Email')
-        cy.get(':nth-child(3) > .form-control').should('have.attr', 'placeholder', 'Password')
-        cy.get('.custom-control-label').contains('Remember Me')
-        cy.get('.btn-primary').contains('Login').should('not.be.disabled')
-        cy.get('.btn-google').contains('Login with Google').should('not.be.disabled')
-        cy.get('.btn-facebook').contains('Login with Facebook').should('not.be.disabled')
-        cy.get('.p-5 > :nth-child(4) > .small').contains('Forgot Password?')
-        cy.get(':nth-child(5) > .small').contains('Create an Account!')
-        cy.get('.d-none').should('be.visible')
+    // //TC-001
+    // it('user can access login page', () => {
+    //     cy.get('.h4').contains('Welcome Back!')
+    //     cy.get(':nth-child(2) > .form-control').should('have.attr', 'placeholder', 'Email')
+    //     // cy.get(':nth-child(3) > .form-control').should('have.attr', 'placeholder', 'Password')
+    //     cy.get('.custom-control-label').contains('Remember Me')
+    //     cy.get('.btn-primary').contains('Login').should('not.be.disabled')
+    //     cy.get('.btn-google').contains('Login with Google').should('not.be.disabled')
+    //     cy.get('.btn-facebook').contains('Login with Facebook').should('not.be.disabled')
+    //     cy.get('.p-5 > :nth-child(4) > .small').contains('Forgot Password?')
+    //     cy.get(':nth-child(5) > .small').contains('Create an Account!')
+    //     cy.get('.d-none').should('be.visible')
+    // })
+
+    //TC-002
+    it('user can login using valid credentials', () => {
+        cy.get(':nth-child(2) > .form-control').type('suyatno@gmail.com')
+        cy.get(':nth-child(3) > .form-control').type('password')
+        cy.get('.btn-primary').contains('Login').click()
+
+        cy.url().should('contain', '/')
+        cy.get('.active > .nav-link').contains('Home')
+        cy.get('.navbar-nav > :nth-child(2) > .nav-link').contains('Our Products')
+        cy.get(':nth-child(3) > .nav-link').contains('About Us')
+        cy.get(':nth-child(4) > .nav-link').contains('Contact Us')
+        cy.get('.border-0 > .nav-link').contains('Logout')
+        cy.get('.latest-products > .container > .row > .col-md-12 > .section-heading > h2').contains('Latest Products')
+        cy.get('.best-features > .container > .row > .col-md-12 > .section-heading > h2').contains('About Brownies Santri')
     })
 })
