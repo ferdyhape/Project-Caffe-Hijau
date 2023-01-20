d647720bdcb128891e26dbd2083e3f1f242ea574
ferdyhape <ferdyhahan5@gmail.com>
2023-01-21 02:03:14 +0700

    beautify the appearance and fixing of login feature

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,59 +8,28 @@
 
             <!-- Outer Row -->
             <div class="row justify-content-center">
-
-                <div class="col-xl-10 col-lg-12 col-md-9 p-0">
-                    @if ($message = Session::get('success'))
-                    <div class="alert alert-success alert-dismissible fade show" role="alert">
-                        {{ $message }}
-                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    @endif
-
-
-                    @if ($message = Session::get('LoginFailed'))
-                    <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                        {{ $message }}
-                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    @endif
-                </div>
                 <div class="col-xl-10 col-lg-12 col-md-9 p-0">
 
                     <div class="card o-hidden border-0 shadow-lg">
                         <div class="card-body p-0">
                             <!-- Nested Row within Card Body -->
-                            <div class="row">
-                                <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+                            <div class="row justify content-center">
+                                <div class="col-lg-6"><img src="{{asset('assets/corp-assets/illustration/login.jpg')}}"
+                                        class="bg-login-register" alt="login-illustration"></div>
                                 <div class="col-lg-6">
-                                    <div class="p-5">
+                                    <div class="col row px-3 py-5 mx-auto">
                                         <div class="text-center">
                                             <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
                                         </div>
                                         <form method="POST" action="/login" class="user">
                                             @csrf
                                             <div class="form-group">
-                                                <input type="email"
-                                                    class="form-control form-control-user @error('email') is-invalid @enderror"
-                                                    name="email" placeholder="Email" required autofocus>
-                                                @error('email')
-                                                <div class="invalid-feedback">
-                                                    {{ $message }}
-                                                </div>
-                                                @enderror
+                                                <input type="email" class="form-control form-control-user " name="email"
+                                                    placeholder="Email" required autofocus>
                                             </div>
                                             <div class="form-group">
                                                 <input type="password" class="form-control form-control-user"
                                                     name="password" placeholder="Passowrd" required>
-                                                @error('password')
-                                                <div class="invalid-feedback">
-                                                    {{ $message }}
-                                                </div>
-                                                @enderror
                                             </div>
                                             <div class="form-group">
                                                 <div class="custom-control custom-checkbox small">
@@ -77,16 +46,14 @@
                                                 class="btn btn-google btn-user btn-block">
                                                 <i class="fab fa-google fa-fw"></i> Login with Google
                                             </a>
-                                            <a href="index.html" class="btn btn-facebook btn-user btn-block">
-                                                <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
-                                            </a>
                                         </form>
-                                        <hr>
-                                        <div class="text-center">
-                                            <a class="small" href="forgot-password.html">Forgot Password?</a>
-                                        </div>
-                                        <div class="text-center">
-                                            <a class="small" href="{{ url('register') }}">Create an Account!</a>
+                                        <div class="my-2">
+                                            <div class="text-center">
+                                                <a class="small" href="forgot-password.html">Forgot Password?</a>
+                                            </div>
+                                            <div class="text-center">
+                                                <a class="small" href="{{ url('register') }}">Create an Account!</a>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
@@ -100,6 +67,27 @@
 
         </div>
 
+        @if ($message = Session::get('toast_error'))
+        <script src="vendor/sweetalert/sweetalert.all.js"></script>
+        <script>
+            const Toast = Swal.mixin({
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+            toast.addEventListener('mouseenter', Swal.stopTimer)
+            toast.addEventListener('mouseleave', Swal.resumeTimer)
+            }
+            })
+            
+            Toast.fire({
+            icon: 'error',
+            title: 'login failed, wrong email or password!'
+            })
+        </script>
+        @endif
         <!-- Bootstrap core JavaScript-->
         <script src="vendor/jquery/jquery.min.js"></script>
         <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
