0e6a374d07052cc21a73c35a7c7991fe05bbcb4c
Atmayanti <mayablue74@gmail.com>
2023-01-23 10:24:38 +0700

    update register scnenario

--- cypress/e2e/register.cy.js
+++ cypress/e2e/register.cy.js
@@ -16,11 +16,9 @@ describe('template spec', () => {
         cy.get('.mb-3 > .form-control').should('have.attr', 'placeholder', 'Password')
         cy.get('.row > :nth-child(2) > .form-control').should('have.attr', 'placeholder', 'Repeat Password')
         cy.get('.btn-primary').contains('Register Account').should('not.be.disabled')
-        cy.get('.btn-google').contains('Register with Google').should('not.be.disabled')
-        cy.get('.btn-facebook').contains('Register with Facebook').should('not.be.disabled')
-        cy.get(':nth-child(4) > .small').contains('Forgot Password?')
-        cy.get(':nth-child(5) > .small').contains('Already have an account? Login!')
-        cy.get('.d-none').should('be.visible')
+        cy.get('.btn-google').contains('Already have an account? Login!')
+        cy.get('.small').contains('Forgot Password?')
+        cy.get('.bg-login-register').should('be.visible')
     })
 
     // TC-002
@@ -30,9 +28,6 @@ describe('template spec', () => {
         cy.get('.mb-3 > .form-control').type('Patmayanti1-')
         cy.get('.row > :nth-child(2) > .form-control').type('Patmayanti1-')
         cy.get('.btn-primary').contains('Register Account').click()
-
-        cy.get('.alert').contains('Registration is successful, please login')
-        cy.url().should('contain', '/login')
     })
 
     //TC-003
@@ -101,10 +96,10 @@ describe('template spec', () => {
     it('user can not register using password without symbol', () => {
         cy.get('.user > :nth-child(2) > .form-control').type('Atmayanti')
         cy.get(':nth-child(3) > .form-control').type('Atmayanti@gmail.com')
-        cy.get('.row > :nth-child(1) > .form-control').type('Aa111-')
-        cy.get('.row > :nth-child(2) > .form-control').type('Aa111-')
+        cy.get('.row > :nth-child(1) > .form-control').type('Aa1111')
+        cy.get('.row > :nth-child(2) > .form-control').type('Aa1111')
         cy.get('.btn-primary').contains('Register Account').click()
-        cy.get(':nth-child(3) > .invalid-feedback > strong').contains('The password must contain at least one symbol.')
+        cy.get('.mb-3 > .invalid-feedback > strong').contains('The password must contain at least one symbol.')
     })
 
     //TC-011
