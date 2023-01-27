8ea04d68380b4a2a61c538014b2522f5d7fa7b1f
Ferdy Hahan Pradana <75787853+ferdyhape@users.noreply.github.com>
2023-01-27 22:42:19 +0700

    update content of home

--- resources/views/client-side/index.blade.php
+++ resources/views/client-side/index.blade.php
@@ -82,19 +82,9 @@
       </div>
       <div class="col-md-6">
         <div class="left-content">
-          <h4>Looking for the best products?</h4>
-          <p><a rel="nofollow" href="https://templatemo.com/tm-546-sixteen-clothing" target="_parent">This
-              template</a> is free to use for your business websites. However, you have no permission to
-            redistribute
-            the downloadable ZIP file on any template collection website. <a rel="nofollow"
-              href="https://templatemo.com/contact">Contact us</a> for more info.</p>
-          <ul class="featured-list">
-            <li><a href="#">Lorem ipsum dolor sit amet</a></li>
-            <li><a href="#">Consectetur an adipisicing elit</a></li>
-            <li><a href="#">It aquecorporis nulla aspernatur</a></li>
-            <li><a href="#">Corporis, omnis doloremque</a></li>
-            <li><a href="#">Non cum id reprehenderit</a></li>
-          </ul>
+          <h4>Apa itu Brownies Santri?</h4>
+          <p>Brownies santri adalah usaha perorangan yang menjual brownies dengan kualitas yang terjamin nikmat, adapun
+            product dari brownies santri terdapat pilihan dan juga bisa request oleh pemesan</p>
           <a href="{{ url('about')}}" class="filled-button">Read More</a>
         </div>
       </div>
